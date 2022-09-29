8f0b2ec87f46772806303009800fd904c63a8fbe
Wilfredo Pena <penawebservices@gmail.com>
2022-09-29 11:00:24 -0400

    Update streamlit_app.py
    Making the fruit name the index for the multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import streamlit as st
 import streamlit
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
 
