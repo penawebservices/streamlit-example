cc6e931278c36a36ec16db1c4a577afd97860139
Wilfredo Pena <penawebservices@gmail.com>
2022-09-29 10:57:12 -0400

    Update streamlit_app.py
    Imported csv file from s3, displaying it under the build your own fruit smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ import streamlit as st
 
 import streamlit
 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -15,6 +17,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
 
 
 """
